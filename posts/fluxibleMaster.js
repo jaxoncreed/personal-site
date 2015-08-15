54f093f5f9fcb4248eeaded3318e8cef3a792095
Jackson Morgan <jmorgan45@gatech.edu>
2015-08-15 15:05:33 -0400

    CD

--- posts/fluxibleMaster.js
+++ posts/fluxibleMaster.js
@@ -6,7 +6,7 @@ var FluxibleMaster = React.createClass({
             <div>
                 <p>You may view the work in progress version of the fluxible tutorial on my <a href="http://jacksoncmorgan.blogspot.com/">blogspot</a>.</p>
                 <p>The blog is somewhat complete. You can get a pretty good idea of fluxible from it and even build a complete site. But, I don't think I explained some things as well as I could have, and fluxible just went through an update that I want to address, so until I'm happy with the post, it won't be displayed here.</p>
-                <p>I like this CD stuff!</p>
+                <p>Let's hope it works</p>
             </div>
         );
     }
